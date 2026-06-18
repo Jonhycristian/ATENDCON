2e268a88891fea789d4a8c621c7c50f80bed09cd
Jonhycristian <wketapi@gmail.com>
2026-06-18 11:41:55 -0300

    chore: remove temporary scratch scripts from root

--- fix.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/App.jsx', 'utf8');
-c = c.replace(/https:\/\/wa\.me\/5531983858280/g, 'https://api.whatsapp.com/send/?phone=5531983858280');
-c = c.replace(/\?text=/g, '&text=');
-c = c.replace(/encodeURIComponent\((.*?)\)/g, 'encodeURIComponent($1) + "&type=phone_number&app_absent=0"');
-c = c.replace(/href=\{WHATSAPP_LINK\}/g, 'href={`${WHATSAPP_LINK}&type=phone_number&app_absent=0`}');
-fs.writeFileSync('src/App.jsx', c);
